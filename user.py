b2b448ff7211a01b85317ecd7032a7d052261baa
cccoast <cccoast@github.io>
2017-02-23 14:49:20 +0800

    hash

--- user.py
+++ user.py
@@ -22,9 +22,26 @@ class User(dbBase.DB_BASE):
     def create_table(self):
         self.user_struct = self.quick_map(self.table_struct)
         
+    def insert_user(self,name,pwd):
+        indict = {'name':name,'password':myhash.generate_password_hash(pwd)}
+        user.insert_dictlike(user.user_struct,indict)
+        
+    def check_user(self,name,pwd):
+        session = self.get_session()
+        re = session.query(self.user_struct).filter_by(name = name).all()
+        ret = False
+        for i in re:
+            if myhash.check_password_hash(i.password, pwd):
+                ret = True
+            else:
+                ret = False
+        session.close()
+        return ret
+        
 if __name__ == '__main__':
     user = User()
     user.create_table()
-    indict = {'name':'xudi','password':myhash.generate_password_hash('123456')}
-    user.insert_dictlike(user.user_struct,indict)
+    user.insert_user('lyx','123456')
+    print user.check_user('lyx','123456')
+    
     
