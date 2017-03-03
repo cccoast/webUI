eac16ea9c85f87dbbf5fb39cc89e3c6753770991
xudi <xudi1989@ruc.edu.cn>
2017-03-03 20:56:33 +0800

    ok

--- app/models.py
+++ app/models.py
@@ -3,27 +3,27 @@ from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
 from flask_login import UserMixin
 from app import db, login_manager
- 
+  
 class User(UserMixin, db.Model):
     __tablename__ = 'password'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
- 
+  
     @property
     def password(self):
         raise AttributeError('password is not a readable attribute')
- 
+  
     @password.setter
     def password(self, password):
         self.password_hash = generate_password_hash(password)
- 
+  
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
- 
+  
     def __repr__(self):
         return '<User %r>' % self.username
-     
+      
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
@@ -57,7 +57,7 @@ if __name__ == '__main__':
     
     indict = {'id':1,'username':'xudi','password_hash':generate_password_hash('123456')}
     user.insert_dictlike(user.user_struct,indict)
-    indict = {'id':0,'username':'lyx','password_hash':generate_password_hash('123456')}
+    indict = {'id':2,'username':'lyx','password_hash':generate_password_hash('123456')}
     user.insert_dictlike(user.user_struct,indict)
     
     
