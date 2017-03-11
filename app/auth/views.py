15853b4aecf9281730300fcde3b7bab002249dc0
xudi <xudi1989@ruc.edu.cn>
2017-03-11 18:47:47 +0800

    cross and right

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,10 +1,23 @@
-from flask import render_template, redirect, request, url_for, flash
+from flask import render_template, redirect, request, url_for, flash, session, g 
 from flask_login import login_user, logout_user, login_required, \
     current_user
 from . import auth
 from .. import db
 from ..models import User
 from .forms import LoginForm,SubmitForm
+
+@auth.context_processor
+def inject_user():
+    if session.has_key('verify'):
+        return dict(verify = session['verify'])
+
+def init_session(_session):
+    _session['verify'] = {}
+    _session['verify']['data'] = True
+    _session['verify']['global'] = False
+    _session['verify']['entry'] = False
+    _session['verify']['exit'] = False
+    _session['verify']['set'] = False
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -13,6 +26,7 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
+            init_session(session)
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('Invalid username or password.')
     return render_template('auth/login.html', form=form)
@@ -31,6 +45,7 @@ def fill():
     sub_form = SubmitForm()
     if sub_form.validate_on_submit():
         flash('Ready to back Testing')
+        print session['verify']
     return render_template('auth/fill.html',submit_form = sub_form)
 
 
