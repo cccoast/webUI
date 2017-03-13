03e64d054499c1fb62fead694b20f7942aa76ad9
xudi <xudi1989@ruc.edu.cn>
2017-03-13 22:10:14 +0800

    add remember me

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -9,6 +9,7 @@ class LoginForm(FlaskForm):
     
     username = StringField('UserName', validators=[Required(), Length(1, 64)])
     password = PasswordField('Password', validators=[Required(), Length(1, 64)])
+    remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log In')
            
 class DataForm(FlaskForm):
