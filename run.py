edddc12d34485232d198dbcb73030e84ec7009be
xudi <xudi1989@ruc.edu.cn>
2017-05-21 12:37:38 +0800

    all var rely on generate_data_block

--- run.py
+++ run.py
@@ -1,5 +1,13 @@
 import os
 from app import xapp
+import sys
+upper_abs_path = os.path.sep.join((os.path.abspath(os.curdir).split(os.path.sep)[:-1]))
+pkg_path = os.path.join(upper_abs_path,'generate_data_block')
+# print pkg_path
+if pkg_path not in sys.path:
+    sys.path.append(pkg_path)
+
+from transfer import server_addr
 
 if __name__ == '__main__':
-    xapp.run('127.0.0.1',5000,True)
+    xapp.run(server_addr,5000,True)
