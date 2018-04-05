b3afcba7d040441b8eb015bcf315603ecd0cf0f7
xudi <xudi1989@ruc.edu.cn>
2018-04-05 09:52:08 +0800

    update password table

--- app/create_db_schema.py
+++ app/create_db_schema.py
@@ -1,7 +1,9 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 
 import sys
-sys.path.append('../../')
+if '../../' not in sys.path:
+    sys.path.append('../../')
+    
 from future_mysql import dbBase
 from sqlalchemy import Column, Integer, String, DateTime, Numeric, Index, Float, Boolean
 from sqlalchemy import Table
@@ -14,26 +16,45 @@ class User(dbBase.DB_BASE):
         super(User, self).__init__(db_name)
 
         self.table_struct = Table(table_name, self.meta,
-                                  Column(
-                                      'id',
-                                      Integer,
-                                      primary_key=True,
-                                      autoincrement=False),
+                                  Column('id',Integer,primary_key=True,autoincrement=False),
                                   Column('username', String(64)),
+                                  Column('work_unit',String(64)),
                                   Column('password_hash', String(128)))
 
     def create_table(self):
         self.user_struct = self.quick_map(self.table_struct)
+        
+class Task(dbBase.DB_BASE):
+    
+    def __init__(self):
+        db_name = 'flask'
+        table_name = 'task'
+        super(User, self).__init__(db_name)
 
-if __name__ == '__main__':
+        self.table_struct = Table(table_name, self.meta,
+                                  Column('username', String(64),primary_key=True,),
+                                  Column('work_unit', String(64)),
+                                  Column('task_name',String(64)),
+                                  )
+
+    def create_table(self):
+        self.user_struct = self.quick_map(self.table_struct)
+
+def create_user_table():
     user = User()
     user.create_table()
-    
     indict = {
-        'id': 3,
-        'username': 'guest',
-        'password_hash': generate_password_hash('123456')
+        'id': 1,
+        'username': 'xudi',
+        'password_hash': generate_password_hash('123456'),
+        'work_unit':'pbc'
     }
     user.insert_dictlike(user.user_struct, indict)
     print 'successed!'
+    
+def create_task_table():
+    pass
+
+if __name__ == '__main__':
+    create_user_table()
     
